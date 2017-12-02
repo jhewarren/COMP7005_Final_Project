ed114ea640623c57ae51d6df1dd855c48d80f6bc
John Warren <jhewarren@gmail.com>
2017-12-02 13:29:52 -0800

    added initial noise class

---
+++ src/noise.py
@@ -0,0 +1,21 @@
+import random
+class noise(object):
+
+    @dump_func_name
+    def __init__(self)
+        global total_packets_sent = 0
+
+    @dump_func_name
+    def set_err_rate(self, error_rate)
+
+        global err_pkt
+        global err_rate
+
+        // assume that rate < 1/maxint is negligible
+        if error_rate > sys.maxint:
+            error_rate = sys.maxint
+        err_pkt = random.randint(1,error_rate)
+
+    @dump_func_name
+    def is_packet_lost(self)
+        self.lost = (err_pkt == total_packets_sent %= err_rate)
