97be4453f79ee87478f58730dfddac2270da7195
Dimitry Rakhlei <dimitryrakhlei@gmail.com>
2017-12-02 15:12:04 -0800

    refactoring the noise functions

--- src/noise.py
+++ src/noise.py
@@ -1,21 +1,37 @@
 import random
+from debug import dump_func_name
+import sys
+
+
 class noise(object):
 
     @dump_func_name
-    def __init__(self)
-        global total_packets_sent = 0
+    def __init__(self, error_rate):
+        self.total_packets_sent = 0
+        self.set_err_rate(error_rate)
 
     @dump_func_name
-    def set_err_rate(self, error_rate)
+    def set_err_rate(self, error_rate):
+        self.err_rate = error_rate
+        # assume that rate < 1 / maxint is negligible
+        if self.err_rate > sys.maxsize:
+            self.err_rate = sys.maxsize
+        self.err_pkt = random.randint(1, self.err_rate)
 
-        global err_pkt
-        global err_rate = error_rate
+    #@dump_func_name
+    def is_packet_lost(self):
+        is_lost = True if self.err_pkt == (self.total_packets_sent % self.err_rate) else False
+        if is_lost is True:
+            return True
+        else:
+            self.total_packets_sent += 1
+            return False
 
-        // assume that rate < 1/maxint is negligible
-        if error_rate > sys.maxint:
-            error_rate = sys.maxint
-        err_pkt = random.randint(1,err_rate)
 
-    @dump_func_name
-    def is_packet_lost(self)
-        self.lost = (err_pkt == total_packets_sent %= err_rate)
+if __name__ == "__main__":
+    # lose 1 in 500
+    n = noise(10)
+    for i in range(10):
+        f = n.is_packet_lost()
+        if f is True:
+            print(f, i, "packet lost")
